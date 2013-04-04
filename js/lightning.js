279ae655f6746f56cb5fb8ba4100ea2d9ee766bf
blissdev <blissdev@gmail.com>
2013-04-04 04:41:59 -0500

    initial attempt at playing songs

--- js/lightning.js
+++ js/lightning.js
@@ -1,3 +1,26 @@
+function Player(sounds) {
+  this.sounds = sounds;
+  SC.whenStreamingReady(this.playRandom);
+}
+
+Player.prototype.playRandom = function() {
+  var player = this;
+  console.log('playing a random sound');
+  SC.stream(this.getRandomSound(), {
+    autoPlay: true,
+    onfinish: function() {
+      player.playRandom();
+
+      console.log('the song is over :(');
+    }
+  });
+}
+
+Player.prototype.getRandomSound = function() {
+  var index = Math.floor(Math.random() * this.sounds.length);
+  return this.sounds[index].stream_url;
+}
+
 SC.initialize({
   client_id: 'd214e0bc838d15c8b31cca256119cb23',
   redirect_uri: 'http://blissdev.github.com/lightning/callback.html'
@@ -6,7 +29,6 @@ SC.initialize({
 // initiate auth popup
 SC.connect(function() {
   SC.get('/me/favorites', { limit: 200 }, function(favorites) {
-    window.favorites = favorites;
-    console.log('favorites set');
+    window.player = new Player(favorites);
   });
 });
